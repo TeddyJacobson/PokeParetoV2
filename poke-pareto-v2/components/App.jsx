1f876e44cf64ac01e45e069bbcfccd1ddc641cd4
Teddy Jacobson <theodorejacobson64@gmail.com>
2026-06-09 18:34:47 -0400

    Access a list of all pokemon

--- poke-pareto-v2/components/App.jsx
+++ poke-pareto-v2/components/App.jsx
@@ -1,8 +1,33 @@
-export default function App()
-{
-    return(
-        <h1>
-            Testers
-        </h1>
+import { PokeDex } from "@atosjs/pokemon";
+import { useState, useEffect } from "react"
+
+export default function App() {
+    // A list of every pokemon
+    const [allPokes, setAllPokes] = useState(null)
+
+    // Accessing all the pokemon we want
+    const pokedex = new PokeDex()
+    useEffect(() => {
+
+        async function getMons() {
+            // NOTE: CURRENT LIST IS HARD CODED FOR UP TO GEN 9 DLC!
+            let pokeList = await pokedex.fetch({ id: [...Array(1026).keys()].splice(1) })
+            setAllPokes(pokeList)
+        }
+
+        getMons();
+
+    }, [])
+
+
+    return (
+        <>
+            <h1>
+                Pokemon Pareto V2
+            </h1>
+            {allPokes && <>
+                <h2>This is a test</h2>
+            </>}
+        </>
     )
 }
